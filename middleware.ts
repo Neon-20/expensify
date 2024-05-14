5c608b1468a3612c784cab7c6786bc03c7032ba9
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-15 01:19:46 +0530

    feat: added middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,31 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-    publicRoutes:["/"],
-});
+// export default authMiddleware({
+//     publicRoutes:["/"],
+// });
+
+// export const config = {
+// matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+const isProtectedRoute = createRouteMatcher([
+  // This is a protected route
+  '/dashboard'
+]);
+
+
+export default clerkMiddleware((auth,req)=>{
+  if(isProtectedRoute(req)){
+    auth().protect();
+  }
+  return NextResponse.next();
+})
 
 export const config = {
-matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
