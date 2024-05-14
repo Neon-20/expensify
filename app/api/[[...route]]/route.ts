6c500eb830482468720857d845f3bb1368104a6f
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-15 01:26:35 +0530

    feat: introduced hono-apis

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,13 +1,14 @@
 import {Hono} from "hono";
 import {handle} from "hono/vercel";
 
+
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 app.get("/hello",(c)=> {
     return c.json({
-        hello:"hono"
+        hello:"world"
     })
 })
 
